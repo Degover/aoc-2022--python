c896d5ddf294fb41220c1ab90dc3ff26a27d3fd4
Degover <chaors2000@gmail.com>
2022-12-16 08:14:19 -0300

    Added initial files to solve day 10

---
+++ src/d10/solution.py
@@ -0,0 +1,14 @@
+def part1(input) -> str:
+    return "Not implemented yet"
+
+
+def part2(input) -> str:
+    return "Not implemented yet"
+
+
+def solve_d10():
+    with open('inputs/7.txt', 'r') as file_input:
+        print(part1(file_input))
+
+    with open('inputs/7.txt', 'r') as file_input:
+        print(part2(file_input))
---
+++ src/tests/d10/solution_test.py
@@ -0,0 +1,21 @@
+from src.d10.solution import *
+from src.tests.utils.file_stub import FileStub
+from src.tests.d10.example import example
+
+
+def test_Part1_WithExample_ShouldBeCorrect():
+    file_stub = FileStub()
+    file_stub.set_array(example)
+
+    result = part1(file_stub)
+
+    assert result == "13140"
+
+
+def test_Part2_WithExample_ShouldBeCorrect():
+    file_stub = FileStub()
+    file_stub.set_array(example)
+
+    result = part2(file_stub)
+
+    assert result == "Not implemented yet"
